023ad0c5d90b77f0b38766f90ebbceb283e85597
Tom Spurling <tom@tspurling.co.uk>
2022-04-01 16:32:04 -0400

    Remove handler mode stuff

--- certbot_dns_json/plugin.py
+++ certbot_dns_json/plugin.py
@@ -3,8 +3,6 @@
 
 import json
 import logging
-import os
-import subprocess
 import sys
 import datetime
 
@@ -59,7 +57,6 @@ class Authenticator(common.Plugin, interfaces.Authenticator):
 
     def __init__(self, *args, **kwargs):
         super(Authenticator, self).__init__(*args, **kwargs)
-        self._handler_file_problem = False
 
         # Set up reverter
         self.reverter = reverter.Reverter(self.config)
@@ -69,17 +66,11 @@ class Authenticator(common.Plugin, interfaces.Authenticator):
     def add_parser_arguments(cls, add):
         add("test-mode", action="store_true",
             help="Test mode. Executes the manual command in subprocess.")
-        add("handler", default=None,
-            help="Handler program that takes the action. Data is transferred in ENV vars")
-        add("dehydrated-dns", action="store_true",
-            help="Switches handler mode to Dehydrated DNS compatible version")
 
     def prepare(self):  # pylint: disable=missing-docstring,no-self-use
         # Non-interactive not yet supported
         if self.config.noninteractive_mode and not self.conf("test-mode"):
             raise errors.PluginError("Running manual mode non-interactively is not supported (yet)")
-        if not self._is_handler_mode() and self._is_dehydrated_dns():
-            raise errors.PluginError("dehydrated-dns switch is allowed only with handler specified")
 
     def more_info(self):  # pylint: disable=missing-docstring,no-self-use
         return ("It's like --manual for DNS challenges except it prints less.")
@@ -100,14 +91,8 @@ class Authenticator(common.Plugin, interfaces.Authenticator):
         # TODO: group achalls by the same socket.gethostbyname(_ex)
         # and prompt only once per server (one "echo -n" per domain)
 
-        if self._is_classic_handler_mode() and self._call_handler("pre-perform") is None:
-            raise errors.PluginError("Error in calling the handler to do the pre-perform (challenge) stage")
-
         for achall in achalls:
             responses.append(mapping[achall.typ](achall))
-
-        if self._is_classic_handler_mode() and self._call_handler("post-perform") is None:
-            raise errors.PluginError("Error in calling the handler to do the post-perform (challenge) stage")
 
         input("Press return to verify...\n")
         return responses
@@ -131,94 +116,6 @@ class Authenticator(common.Plugin, interfaces.Authenticator):
         self._json_out({'challenge':json_data}, True)
 
         return response
-
-    #
-    # Caller
-    #
-
-    def _call_handler(self, command, *args, **kwargs):
-        """
-        Invoking the handler script
-        :param command:
-        :param args:
-        :param kwargs:
-        :return:
-        """
-        env = dict(os.environ)
-        env.update(kwargs)
-
-        # Dehydrated compatibility mode - translate commands
-        if self._is_dehydrated_dns():
-            auth_cmd_map = {'perform': 'deploy_challenge', 'cleanup': 'clean_challenge'}
-            install_cmd_map = {'deploy_cert': 'deploy_cert', 'unchanged_cert': 'unchanged_cert'}
-
-            if command in auth_cmd_map:
-                command = auth_cmd_map[command]
-                args = list(args) + [kwargs.get(FIELD_DOMAIN), kwargs.get(FIELD_TOKEN), kwargs.get(FIELD_VALIDATION)]
-
-            elif command in install_cmd_map:
-                command = install_cmd_map[command]
-                args = list(args) + [kwargs.get(FIELD_DOMAIN), kwargs.get(FIELD_KEY_PATH), kwargs.get(FIELD_CERT_PATH),
-                                     kwargs.get(FIELD_FULLCHAIN_PATH), kwargs.get(FIELD_CHAIN_PATH)]
-
-                if command == 'deploy_cert':
-                    args.append(kwargs.get(FIELD_TIMESTAMP))
-
-            else:
-                logger.info("Dehydrated mode does not support this handler command: %s" % command)
-
-        proc = None
-        stdout, stderr = None, None
-        arg_list = [self._get_handler(), command] + list(args)
-
-        # Check if the handler exists
-        if not os.path.isfile(self._get_handler()):
-            self._handler_file_problem = True
-            logger.error("Handler script file `%s` not found. Absolute path: %s"
-                         % (self._get_handler(), self._try_get_abs_path(self._get_handler())))
-
-            if os.path.exists(self._get_handler()):
-                logger.error("Handler script `%s` is not a file" % self._get_handler())
-
-            return None
-
-        # Check if is executable
-        # Still try to run, throw an exception only if problem really did occur.
-        exec_problem = not self._is_file_executable(self._get_handler())
-
-        # The handler invocation
-        try:
-            proc = subprocess.Popen(arg_list,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE,
-                                    env=env)
-            stdout, stderr = proc.communicate()
-
-            # Handler processing
-            if proc.returncode != 0:
-                if stdout.strip() == "NotImplemented":
-                    logger.warning("Handler script does not implement the command %s\n - Stderr: \n%s",
-                                   command, stderr)
-                    return NotImplemented
-
-                else:
-                    logger.error("Handler script failed!\n - Stdout: \n%s\n - Stderr: \n%s", stdout, stderr)
-                    return None
-
-            else:
-                    logger.info("Handler output (%s):\n - Stdout: \n%s\n - Stderr: \n%s",
-                                command, stdout, stderr)
-            return stdout
-
-        except Exception as e:
-            self._handler_file_problem = True
-            logger.error("Handler script invocation failed with an exception. \n - Script: %s\n - Exception: %s"
-                         % (' '.join(arg_list), e))
-            if exec_problem:
-                logger.error("Handler script %s does not have the executable permission set so it cannot be executed. "
-                             "\n - Try running: chmod +x \"%s\" " % (self._get_handler(), self._try_get_abs_path(self._get_handler())))
-            else:
-                logger.warning("Make sure the handler file exists and is executable (+x permission on a Posix system)")
 
     #
     # Helper methods & UI
@@ -249,73 +146,6 @@ class Authenticator(common.Plugin, interfaces.Authenticator):
                     nval.append(item)
                 dictionary[key] = nval
         return dictionary
-
-    def _is_file_executable(self, fpath):
-        """
-        Returns true if the given file is executable (+x flag)
-        :param fpath:
-        :return:
-        """
-        if os.name.lower() == 'posix':
-            try:
-                return os.access(fpath, os.X_OK)
-            except:
-                return False
-        else:
-            return True
-
-    def _try_get_abs_path(self, fpath):
-        """
-        Returns absolute path, catching possible exceptions.
-        :param fpath:
-        :return:
-        """
-        try:
-            return os.path.abspath(fpath)
-        except:
-            return fpath
-
-    def _is_json_mode(self):
-        """
-        Returns true if json mode is selected
-        :return:
-        """
-        return not self._is_handler_mode()
-
-    def _is_handler_mode(self):
-        """
-        Returns true if handler mode is selected
-        :return:
-        """
-        return self.conf("handler") is not None
-
-    def _is_handler_broken(self):
-        """
-        Returns true if the handler file cannot be executed - exception was thrown
-        :return:
-        """
-        return self._handler_file_problem
-
-    def _is_classic_handler_mode(self):
-        """
-        Handler mode && not dehydrated
-        :return:
-        """
-        return self._is_handler_mode() and not self._is_dehydrated_dns()
-
-    def _get_handler(self):
-        """
-        Returns handler script path - from CLI argument
-        :return:
-        """
-        return self.conf("handler")
-
-    def _is_dehydrated_dns(self):
-        """
-        Returns true if dehydrated dns mode is used
-        :return:
-        """
-        return self.conf("dehydrated-dns")
 
     def _json_dumps(self, data, **kwargs):
         """
